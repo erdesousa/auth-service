f726a2baba60f9f13a631301c118048d14c6acb3
EricdSouzx <eericdsn@gmail.com>
2025-06-17 13:09:09 -0300

    feat: Criação do projeto com entidade User e integração com H2

---
+++ authservice/src/main/java/com/auth/authservice/entity/User.java
@@ -0,0 +1,29 @@
+package com.auth.authservice.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Email(message = "O email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    private String senha;
+}
