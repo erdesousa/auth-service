d61882bf06c91c8f404171ff07e8b6219f8ea298
EricdSouzx <eericdsn@gmail.com>
2025-06-23 12:38:53 -0300

    feat: criar método para validar token

--- authservice/src/main/java/com/auth/authservice/security/TokenService.java
+++ authservice/src/main/java/com/auth/authservice/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth.authservice.entity.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +28,19 @@ public class TokenService {
             return token;
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro enquanto estava autenticando");
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token )
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
         }
     }
 
