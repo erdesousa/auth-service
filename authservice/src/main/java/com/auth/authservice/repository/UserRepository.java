4136f1701d2db2e0899458188144c35d996857af
EricdSouzx <eericdsn@gmail.com>
2025-06-23 15:20:08 -0300

    feat: implementa classe SecurityFilter para filtro de segurança e método findByEmail na repository para autenticação

--- authservice/src/main/java/com/auth/authservice/repository/UserRepository.java
+++ authservice/src/main/java/com/auth/authservice/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.auth.authservice.repository;
 import com.auth.authservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
